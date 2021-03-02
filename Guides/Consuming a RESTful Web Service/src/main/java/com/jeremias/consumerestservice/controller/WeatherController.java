5aa010cba7fd53e00285b850ea513ddd2090f876
Jereaaa <jere_calvet@hotmail.com>
2021-03-02 18:50:48 -0300

    brb

--- Guides/Consuming a RESTful Web Service/src/main/java/com/jeremias/consumerestservice/controller/WeatherController.java
+++ Guides/Consuming a RESTful Web Service/src/main/java/com/jeremias/consumerestservice/controller/WeatherController.java
@@ -1,15 +1,12 @@
 package com.jeremias.consumerestservice.controller;
 
-import com.jeremias.consumerestservice.domain.WeatherDTO;
 import com.jeremias.consumerestservice.service.WeatherService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("api/v1/weather")
@@ -19,13 +16,7 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @GetMapping
-    public ResponseEntity<WeatherDTO> askCurrentWeather(@RequestParam("cityToCheck") String cityToCheck) {
-        final ResponseEntity<WeatherDTO> currentWeatherResponse = weatherService.getCurrentWeather(cityToCheck);
-
-        if (currentWeatherResponse != null) {
-            return currentWeatherResponse;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("City %s not found.", cityToCheck));
-        }
+    public Mono<?> askCurrentWeather(@RequestParam("cityToCheck") String cityToCheck) {
+        return weatherService.getCurrentWeather(cityToCheck);
     }
 }
--- Guides/Consuming a RESTful Web Service/src/main/java/com/jeremias/consumerestservice/service/WeatherService.java
+++ Guides/Consuming a RESTful Web Service/src/main/java/com/jeremias/consumerestservice/service/WeatherService.java
@@ -4,11 +4,10 @@ import com.jeremias.consumerestservice.domain.WeatherDTO;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Service
@@ -17,28 +16,26 @@ public class WeatherService {
     private static final Logger LOG = LoggerFactory.getLogger(WeatherService.class);
     private static final String API_KEY = "RAPIDAPIKEYOPENWEATHERMAP";
 
-    public ResponseEntity<WeatherDTO> getCurrentWeather(String cityToCheck) {
-        final RestTemplate restTemplate = new RestTemplateBuilder().build();
-        final HttpHeaders headers = createHttpHeaders();
-        final HttpEntity<?> request = new HttpEntity<>(headers);
-        String urlTargetAPI = "https://community-open-weather-map.p.rapidapi.com/weather?q=" + cityToCheck;
+    public Mono<?> getCurrentWeather(String cityToCheck) {
+        //create web client
+        WebClient client = WebClient.builder()
+                .baseUrl("https://community-open-weather-map.p.rapidapi.com/weather?q=" + cityToCheck)
+                .defaultHeader("x-rapidapi-key", System.getenv(API_KEY))
+                .defaultHeader("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
+                //.defaultUriVariables(Collections.singletonMap("q", cityToCheck))
+                .build();
 
-        ResponseEntity<WeatherDTO> response = null;
-        try {
-             response = restTemplate.exchange(urlTargetAPI,
-                    HttpMethod.GET,
-                    request,
-                    WeatherDTO.class);
-        } catch (RestClientException e) {
-            LOG.error(String.format("Error: %s Searched city: %s.", e, cityToCheck));
-        }
-        return response;
-    }
 
-    private HttpHeaders createHttpHeaders() {
-        final HttpHeaders headers = new HttpHeaders();
-        headers.set("x-rapidapi-key", System.getenv(API_KEY));
-        headers.set("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com");
-        return headers;
+        //getting response
+        return client.get().exchangeToMono(httpResponse -> {
+            if (httpResponse.statusCode().equals(HttpStatus.OK)) {
+                return httpResponse.bodyToMono(WeatherDTO.class);
+            } else if (httpResponse.statusCode().is4xxClientError()) {
+                return Mono.just(String.format("Error %d: %s not found.", httpResponse.statusCode().value(), cityToCheck));
+            } else {
+                return httpResponse.createException().flatMap(Mono::error);
+            }
+        });
+        //TODO: figure out how to pass the exception to the controller to throw the right status code
     }
 }
