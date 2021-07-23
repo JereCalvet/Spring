b35ab64759d85cd394d25f1fec824276b2ae0673
Jereaaa <jere_calvet@hotmail.com>
2021-07-23 16:13:30 -0300

    terminada clase 3

--- Laurentiu Spilca lessons/Spring Framework - Lesson 3 - It's time to connect to your database/src/main/java/main/Main.java
+++ Laurentiu Spilca lessons/Spring Framework - Lesson 3 - It's time to connect to your database/src/main/java/main/Main.java
@@ -9,13 +9,15 @@ public class Main {
     public static void main(String[] args) {
         try (final var context = new AnnotationConfigApplicationContext(ProjectConfig.class)) {
 
-            Product beer = new Product();
+            /*Product beer = new Product();
             beer.setName("Beer");
-            beer.setPrice(10);
+            beer.setPrice(10);*/
 
             final ProductRepository productRepo = context.getBean(ProductRepository.class);
 
-            productRepo.addProduct(beer);
+            //productRepo.addProduct(beer);
+
+            productRepo.getProducts().forEach(System.out::println);
 
             //https://youtu.be/_t3BbWs5PcI?list=PLEocw3gLFc8WO_HvFzTWUj2fqa7Y8-yg5&t=1737
         }
--- Laurentiu Spilca lessons/Spring Framework - Lesson 3 - It's time to connect to your database/src/main/java/repositories/ProductRepository.java
+++ Laurentiu Spilca lessons/Spring Framework - Lesson 3 - It's time to connect to your database/src/main/java/repositories/ProductRepository.java
@@ -3,12 +3,18 @@ package repositories;
 import domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Repository
 public class ProductRepository {
-/*    Esta clase permite enviar queries a la db sin la necesidad del boiler plate de crear un statement,
-    enviarlo a la db, extraer lo resultados, etc.*/
+    /*    Esta clase permite enviar queries a la db sin la necesidad del boiler plate de crear un statement,
+        enviarlo a la db, extraer lo resultados, etc.*/
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -17,7 +23,32 @@ public class ProductRepository {
     }
 
     public void addProduct(Product product) {
-        String sql = "INSERT INTO Product VALUES (NULL, ?, ?);";
+        String sql = "INSERT INTO Product VALUES (NULL, ?, ?)";
         jdbcTemplate.update(sql, product.getName(), product.getPrice());
+    }
+
+    public List<Product> getProducts() {
+        String sql = "SELECT * FROM Product";
+
+        /*Ejecución de la query y mapeo con clase anonima*/
+        /*final List<Product> selectResult = jdbcTemplate.query(sql, new RowMapper<Product>() {
+
+            @Override
+            public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return mapProduct(rs);
+            }
+        });*/
+
+        /*Ejecución de la query y mapeo con lambda*/
+        final List<Product> selectResult = jdbcTemplate.query(sql, (rs, i) -> mapProduct(rs));
+        return selectResult;
+    }
+
+    private Product mapProduct(ResultSet rs) throws SQLException {
+        final Product result = new Product();
+        result.setId(rs.getInt("idProduct"));
+        result.setName(rs.getString("name"));
+        result.setPrice(rs.getDouble("price"));
+        return result;
     }
 }
